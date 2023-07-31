a566dfe1696287c0921366a8f0e9b0292a6aa07b
João Gabriel <joogps@gmail.com>
2023-07-31 01:15:37 -0300

    Experimento com degradê radial

--- lib/views/usuario_row.dart
+++ lib/views/usuario_row.dart
@@ -12,8 +12,14 @@ class UsuarioRow extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(20),
       child: Container(
-        color: CupertinoColors.systemGrey6,
-        padding: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          gradient: RadialGradient(
+            colors: [CupertinoColors.systemPink, CupertinoColors.systemGrey6],
+            stops: [0.2, 1.0],
+          ),
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 50.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
