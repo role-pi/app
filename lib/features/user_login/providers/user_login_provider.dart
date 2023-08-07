d653402e5bceabcdba9e53ca39014c53bd0574da
João Gabriel <joogps@gmail.com>
2023-08-06 22:58:11 -0300

    Serviço de autenticação funcionando

--- lib/features/user_login/providers/user_login_provider.dart
+++ lib/features/user_login/providers/user_login_provider.dart
@@ -5,27 +5,67 @@ import 'package:role/shared/utils/api.dart';
 import 'package:role/shared/utils/api_status.dart';
 
 class UserLoginProvider extends ChangeNotifier {
-  // Replace this with your API URL.
-  bool _isLoggedIn = false;
+  LoginState _state = LoginState.signUp;
 
-  bool get isLoggedIn => _isLoggedIn;
+  LoginState get state => _state;
 
-  Future<void> tryLogin() async {
-    // Implement a function to get the JWT token from device storage.
-    if (!isLoggedIn) {
+  Future<void> tryAuthentication(callback) async {
+    if (state != LoginState.loggedIn) {
       var response = await API().post("usuario/login", {});
 
       if (response is Success) {
         if (json.decode(response.response as String).containsKey("user")) {
-          _isLoggedIn = true;
+          _state = LoginState.loggedIn;
           notifyListeners();
-          print("AAAAAA");
+          callback();
         }
       } else if (response is Failure) {
-        // Login failed.
-        _isLoggedIn = false;
+        _state = LoginState.signUp;
         notifyListeners();
       }
     }
   }
+
+  Future<void> trySignUp(email, callback) async {
+    if (state != LoginState.loggedIn) {
+      var response = await API().post("usuario/signin", {"email": email});
+
+      if (response is Success) {
+        if (json.decode(response.response as String).containsKey("user")) {
+          _state = LoginState.loggedIn;
+          notifyListeners();
+          callback();
+        }
+      } else if (response is Failure) {
+        _state = LoginState.signUp;
+        notifyListeners();
+      }
+    }
+  }
+
+  Future<void> verify(email, code, callback) async {
+    if (state != LoginState.loggedIn) {
+      var response =
+          await API().post("usuario/verify", {"email": email, "code": code});
+
+      print(response);
+
+      if (response is Success) {
+        if (json.decode(response.response as String).containsKey("user")) {
+          _state = LoginState.loggedIn;
+          notifyListeners();
+          callback();
+        }
+      } else if (response is Failure) {
+        _state = LoginState.signUp;
+        notifyListeners();
+      }
+    }
+  }
+}
+
+enum LoginState {
+  loggingIn,
+  loggedIn,
+  signUp,
 }
