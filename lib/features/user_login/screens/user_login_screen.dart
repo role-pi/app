15a0038ce51be7c82643bfc82d76e681a4372382
caio fabio gregio <caio.f04@aluno.ifsc.edu.br>
2023-08-08 10:44:35 -0300

    Ajustes login

--- lib/features/user_login/screens/user_login_screen.dart
+++ lib/features/user_login/screens/user_login_screen.dart
@@ -34,46 +34,61 @@ class _UserLoginScreenState extends State<UserLoginScreen> {
           primaryColor: CupertinoColors.white,
         ),
         child: CupertinoPageScaffold(
-          child: Form(
-            key: _formKey,
-            child: Column(children: [
-              Spacer(),
-              CupertinoTextFormFieldRow(
-                placeholder: "Email",
-                controller: _emailController,
-                padding: EdgeInsets.all(10),
-                style: TextStyle(color: CupertinoColors.white),
-                validator: (value) {
-                  return validateEmail(value);
-                },
-              ),
-              CupertinoButton(
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    await loginProvider.trySignUp(
-                        _emailController.text, () => {});
-                  }
-                },
-                child: const Text('Entrar'),
-              ),
-              SizedBox(height: 80),
-              CupertinoTextFormFieldRow(
-                  placeholder: "Código",
-                  controller: _codeController,
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Form(
+              key: _formKey,
+             
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Spacer(),
+               
+                  Text("bem-vindo ao seu novo aplicativo de evento",
+                    style: TextStyle(
+                      color: CupertinoColors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 28,
+                    ),
+                  ),
+                
+                CupertinoTextFormFieldRow(
+                  placeholder: "Email",
+                  controller: _emailController,
                   padding: EdgeInsets.all(10),
                   style: TextStyle(color: CupertinoColors.white),
-                  keyboardType: TextInputType.number),
-              CupertinoButton(
-                onPressed: () async {
-                  await loginProvider.verify(
-                      _emailController.text,
-                      _codeController.text,
-                      () => {Navigator.pushNamed(context, "/")});
-                },
-                child: const Text('Verificar'),
-              ),
-              Spacer(),
-            ]),
+                  validator: (value) {
+                    return validateEmail(value);
+                  },
+                ),
+                CupertinoButton(
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      await loginProvider.trySignUp(
+                          _emailController.text, () => {});
+                    }
+                  },
+                  child: const Text('Entrar'),
+                ),
+                SizedBox(height: 80),
+                CupertinoTextFormFieldRow(
+                    placeholder: "Código",
+                    controller: _codeController,
+                    padding: EdgeInsets.all(10),
+                    style: TextStyle(color: CupertinoColors.white),
+                    keyboardType: TextInputType.number),
+                CupertinoButton(
+                  onPressed: () async {
+                    await loginProvider.verify(
+                        _emailController.text,
+                        _codeController.text,
+                        () => {Navigator.pushNamed(context, "/")});
+                  },
+                  child: const Text('Verificar'),
+                ),
+                Spacer(),
+              ]),
+            ),
           ),
           backgroundColor: CupertinoColors.black,
         ),
