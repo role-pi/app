d673c38e4e1899f901adc305b2d26258ed9176c0
Ana Clara Castellain <anaclara.castellain@gmail.com>
2023-08-10 08:50:21 -0300

    Bugo

--- lib/features/event_list/widgets/evento_item_row.dart
+++ lib/features/event_list/widgets/evento_item_row.dart
@@ -101,27 +101,42 @@ class EventoItemRowState extends State<EventoItemRow>
               color2: widget.evento.randomColor2,
               child: Padding(
                 padding: const EdgeInsets.all(12.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                child: Row(
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Text(widget.evento.randomEmoji,
-                          style: TextStyle(
-                              fontSize: 52, fontWeight: FontWeight.bold)),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Text(widget.evento.randomEmoji,
+                              style: TextStyle(
+                                  fontSize: 52, fontWeight: FontWeight.bold)),
+                        ),
+                        Text(widget.evento.name,
+                            style: TextStyle(
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: -1.5,
+                                color: CupertinoColors.white.withAlpha(200))),
+                        Text("em 96 dias",
+                            style: TextStyle(
+                                fontSize: 22,
+                                letterSpacing: -1.5,
+                                color: CupertinoColors.white.withAlpha(150))),
+                      ],
                     ),
-                    Text(widget.evento.name,
-                        style: TextStyle(
-                            fontSize: 28,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: -1.5,
-                            color: CupertinoColors.white.withAlpha(200))),
-                    Text("em 96 dias",
-                        style: TextStyle(
-                            fontSize: 22,
-                            letterSpacing: -1.5,
-                            color: CupertinoColors.white.withAlpha(150))),
+                    Spacer(),
+                    Column(
+                      children: [
+                        Text("R\$ 120,00",
+                            style: TextStyle(
+                                fontSize: 22,
+                                letterSpacing: -1.5,
+                                color: CupertinoColors.white.withAlpha(150))),
+                        Spacer(),
+                      ],
+                    ),
                   ],
                 ),
               ))),
