9b92772afacd7e5dfe1c1d5a8d543f1f1edd49e7
caio fabio gregio <caio.f04@aluno.ifsc.edu.br>
2023-08-10 00:13:22 -0300

    edição fim
    ta lindu menino de papai

--- lib/features/user_login/widgets/verification_widget.dart
+++ lib/features/user_login/widgets/verification_widget.dart
@@ -20,24 +20,38 @@ class VerificationWidget extends StatelessWidget {
           Text(
             "Otimo! agora só precisamos do codigo de verificação que enviamos no seu email",
             style: TextStyle(
-              color: Color.fromARGB(255, 72, 72, 72),
+              color: Color.fromARGB(255, 211, 209, 209),
               fontWeight: FontWeight.bold,
               fontSize: 25,
             ),
           ),
-          CupertinoTextFormFieldRow(
-            placeholder: "Código",
-            controller: _codeController,
-            padding: EdgeInsets.all(10),
-            style: TextStyle(color: CupertinoColors.white),
-            keyboardType: TextInputType.number,
+          SizedBox(height: 22), 
+          Container(
+          decoration: BoxDecoration(
+           color: Color.fromARGB(255, 62, 62, 62), // Cor de fundo cinza escuro
+           borderRadius: BorderRadius.circular(10), // Raio de borda para arredondamento
+          ),
+          padding: EdgeInsets.all(4),
+         child: CupertinoTextFormFieldRow(
+        placeholder: "Código",
+         controller: _codeController,
+         style: TextStyle(color: CupertinoColors.white),
+          keyboardType: TextInputType.number,
+         ),
           ),
           SizedBox(height: 25), 
-          RoundButton(
-            onPressed: () async {
-              onTap?.call(_codeController.text);
-            },
-            text: 'verificar',
+          Container(
+          decoration: BoxDecoration(
+           color: Color.fromARGB(255, 169, 167, 167), // Cor de fundo cinza escuro
+          borderRadius: BorderRadius.circular(10), // Raio de borda para arredondamento
+          ),
+          child: RoundButton(
+         onPressed: () async {
+          onTap?.call(_codeController.text);
+         },
+         text: 'verificar',
+         textColor: const Color.fromARGB(255, 243, 243, 243),
+          ),
           ),
           Spacer(),
           Text(
