faa203b007bc64c58b2c5643ba0cae170a792625
João Gabriel <joogps@gmail.com>
2023-08-22 09:06:24 -0300

    Adicionar getters e setters para insumo e usuário

--- lib/models/insumo.dart
+++ lib/models/insumo.dart
@@ -1,21 +1,49 @@
 import 'dart:math';
-
 import 'package:role/models/transacao.dart';
 
 class Insumo {
-  late int valor;
-  late int idInsumo;
-  late int tipo;
-  late String nome;
-  late int idEvento;
+  late int _id;
+  late int _valor;
+  late int _tipo;
+  late String _nome;
+  late int _idEvento;
   List<Transacao> transacoes = [];
 
   Insumo({
-    required this.idInsumo,
-    required this.tipo,
-    required this.nome,
-    required this.idEvento,
-  }) : valor = Random().nextInt(150) + 100;
+    required int id,
+    required int tipo,
+    required String nome,
+    required int idEvento,
+  })  : _id = id,
+        _tipo = tipo,
+        _nome = nome,
+        _idEvento = idEvento,
+        _valor = Random().nextInt(150) + 100;
+
+  int get id => _id;
+  set id(int value) {
+    _id = value;
+  }
+
+  int get valor => _valor;
+  set valor(int value) {
+    _valor = value;
+  }
+
+  int get tipo => _tipo;
+  set tipo(int value) {
+    _tipo = value;
+  }
+
+  String get nome => _nome;
+  set nome(String value) {
+    _nome = value;
+  }
+
+  int get idEvento => _idEvento;
+  set idEvento(int value) {
+    _idEvento = value;
+  }
 
   void addTransacao(Transacao transacao) {
     transacoes.add(transacao);
