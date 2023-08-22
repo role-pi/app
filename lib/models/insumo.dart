4c59792ad41f23bc51120b5706ac61511e97a9db
caio fabio gregio <caio.f04@aluno.ifsc.edu.br>
2023-08-21 23:00:44 -0300

    pensando

--- lib/models/insumo.dart
+++ lib/models/insumo.dart
@@ -3,13 +3,7 @@ import 'dart:math';
 import 'package:role/models/transacao.dart';
 
 class Insumo {
-  late double valor;
-
-  Insumo() {
-    // random between 100 and 250
-    valor = Random().nextInt(150) + 100;
-  }
-
+  late int valor;
   late int idInsumo;
   late int tipo;
   late String nome;
@@ -17,6 +11,7 @@ class Insumo {
   List<Transacao> transacoes = [];
 
   Insumo({
+    valor = Random().nextInt(150) + 100;
     required this.idInsumo,
     required this.tipo,
     required this.nome,
