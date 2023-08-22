ff69b40f8a91bd0bc2a7fbed983c045e1fa8cdec
caio fabio gregio <caio.f04@aluno.ifsc.edu.br>
2023-08-21 22:41:21 -0300

    ;-;

--- lib/models/insumo.dart
+++ lib/models/insumo.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'transacao.dart';
 
 class Insumo {
   late int valor;
@@ -7,4 +8,21 @@ class Insumo {
     // random between 100 and 250
     valor = Random().nextInt(150) + 100;
   }
+
+
+  int idInsumo;
+  int tipo;
+  String nome;
+  int idEvento;
+  List<Transacao> transacoes = [];
+
+  Insumo({
+    required this.idInsumo,
+    required this.tipo,
+    required this.nome,
+    required this.idEvento,
+  });
 }
+  void addTransacao(Transacao transacao) {
+    transacoes.add(transacao);
+  }
