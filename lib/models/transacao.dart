c88e16e6f1adde7e43d8b052df8b71e830124116
caio fabio gregio <caio.f04@aluno.ifsc.edu.br>
2023-08-21 22:33:38 -0300

    classes

--- lib/models/transacao.dart
+++ lib/models/transacao.dart
@@ -1,1 +1,16 @@
-class Transacao {}
+class Transacao {
+
+  int idTransacao;
+  double valor;
+  DateTime data;
+  int idInsumo;
+  int idUsuario;
+
+  Transacao({
+    required this.idTransacao,
+    required this.valor,
+    required this.data,
+    required this.idInsumo,
+    required this.idUsuario,
+  });
+}
