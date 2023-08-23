c72d05833ecc08883ff10ab8ae2086fa2ee1c209
João Gabriel <joogps@gmail.com>
2023-08-22 21:29:16 -0300

    Animar imagem de fundo

--- lib/features/user_login/screens/user_login_screen.dart
+++ lib/features/user_login/screens/user_login_screen.dart
@@ -33,25 +33,82 @@ class _UserLoginScreenState extends State<UserLoginScreen> {
           primaryColor: CupertinoColors.white,
         ),
         child: CupertinoPageScaffold(
-          child: Padding(
-              padding: const EdgeInsets.all(42.0),
-              child: loginProvider.state == LoginState.signUp
-                  ? SignUpWidget(
-                      onTap: (email) async {
-                        this.email = email;
-                        await loginProvider.trySignUp(email, () {});
-                      },
-                    )
-                  : VerificationWidget(
-                      onTap: (code) async {
-                        await loginProvider.verify(email, code, () {
-                          Navigator.pushNamed(context, "/");
-                        });
-                      },
-                    )),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              AnimatedPositioned(
+                top: loginProvider.state == LoginState.signUp ? 450 : -350,
+                left: 0,
+                right: 0,
+                duration: const Duration(milliseconds: 750),
+                curve: Curves.easeInOutExpo,
+                child: Image.asset(
+                  'assets/Background.png', // Caminho da sua imagem de fundo
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.all(42.0),
+                  child: Stack(
+                    children: [
+                      LoginSlideComponent(
+                          child: SignUpWidget(
+                            onTap: (email) async {
+                              // this.email = email;
+                              // await loginProvider.trySignUp(email, () {});
+                              loginProvider.setState(LoginState.verifying);
+                            },
+                          ),
+                          showing: loginProvider.state == LoginState.signUp),
+                      LoginSlideComponent(
+                          child: VerificationWidget(
+                            onTap: (code) async {
+                              await loginProvider.verify(email, code, () {
+                                Navigator.pushNamed(context, "/");
+                              });
+                            },
+                          ),
+                          showing: loginProvider.state != LoginState.signUp,
+                          reversed: true)
+                    ],
+                  )),
+            ],
+          ),
           backgroundColor: Color.fromARGB(255, 0, 0, 0),
         ),
       );
     }
+  }
+}
+
+class LoginSlideComponent extends StatelessWidget {
+  const LoginSlideComponent({
+    super.key,
+    required this.child,
+    required this.showing,
+    this.reversed = false,
+  });
+
+  final Widget child;
+  final bool showing;
+  final bool reversed;
+
+  Duration get duration => Duration(milliseconds: 750);
+  Curve get curve => Curves.easeInOutExpo;
+
+  @override
+  Widget build(BuildContext context) {
+    return IgnorePointer(
+      ignoring: !showing,
+      child: AnimatedSlide(
+        offset: showing ? Offset.zero : Offset(0, reversed ? 0.1 : -0.1),
+        duration: duration,
+        curve: curve,
+        child: AnimatedOpacity(
+            opacity: showing ? 1 : 0,
+            duration: duration,
+            curve: curve,
+            child: child),
+      ),
+    );
   }
 }
--- lib/features/user_login/widgets/signup_widget.dart
+++ lib/features/user_login/widgets/signup_widget.dart
@@ -14,94 +14,81 @@ class SignUpWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: Stack(
-        children: [
-          Image.asset(
-            'assets/Background.png', // Caminho da sua imagem de fundo
-            fit: BoxFit.cover,
-            width: double.infinity,
-            height: double.infinity,
-          ),
-          Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Spacer(),
-            Image.asset('assets/Invertida.png', height: 60),
-            SizedBox(height: 25),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Text(
-                    "boas-vindas ao seu novo aplicativo de eventos",
-                    style: TextStyle(
-                      color: CupertinoColors.systemGrey5,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 26,
-                      letterSpacing: -1.7,
-                      height: 1.1,
-                    ),
-                  ),
-                  SizedBox(height: 12),
-                  Text(
-                    "para prosseguir, crie ou acesse sua conta",
-                    style: TextStyle(
-                      color: CupertinoColors.systemGrey,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 26,
-                      letterSpacing: -1.7,
-                      height: 1.1,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 40),
-            Container(
-              decoration: BoxDecoration(
-                color:
-                    Color.fromARGB(255, 32, 32, 32), // Cor de fundo cinza escuro
-                borderRadius: BorderRadius.circular(
-                    12), // Raio de borda para arredondamento
-              ),
-              padding: EdgeInsets.all(3),
-              child: CupertinoTextFormFieldRow(
-                placeholder: "digite seu e-mail",
-                controller: _emailController,
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Spacer(),
+        Image.asset('assets/Invertida.png', height: 60),
+        SizedBox(height: 25),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text(
+                "boas-vindas ao seu novo aplicativo de eventos",
                 style: TextStyle(
-                    color: CupertinoColors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: -1.2),
-                validator: (value) {
-                  print("teste");
-                  return validateEmail(value);
-                },
+                  color: CupertinoColors.systemGrey5,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 26,
+                  letterSpacing: -1.7,
+                  height: 1.1,
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                  top: 12), // Adicione o espaçamento desejado acima
-              child: RoundButton(
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    onTap?.call(_emailController.text);
-                  }
-                },
-                textColor: CupertinoColors.black,
-                rectangleColor: CupertinoColors.white,
-                text: 'continuar',
+              SizedBox(height: 12),
+              Text(
+                "para prosseguir, crie ou acesse sua conta",
+                style: TextStyle(
+                  color: CupertinoColors.systemGrey,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 26,
+                  letterSpacing: -1.7,
+                  height: 1.1,
+                ),
               ),
-            ),
-            Spacer()
-          ], //
+            ],
+          ),
         ),
-        ]
-      ), //
+        SizedBox(height: 40),
+        Container(
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 32, 32, 32), // Cor de fundo cinza escuro
+            borderRadius:
+                BorderRadius.circular(12), // Raio de borda para arredondamento
+          ),
+          padding: EdgeInsets.all(3),
+          child: CupertinoTextFormFieldRow(
+            placeholder: "digite seu e-mail",
+            controller: _emailController,
+            style: TextStyle(
+                color: CupertinoColors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                letterSpacing: -1.2),
+            validator: (value) {
+              print("teste");
+              return validateEmail(value);
+            },
+          ),
+        ),
+        Padding(
+          padding:
+              EdgeInsets.only(top: 12), // Adicione o espaçamento desejado acima
+          child: RoundButton(
+            onPressed: () async {
+              if (_formKey.currentState!.validate()) {
+                onTap?.call(_emailController.text);
+              }
+            },
+            textColor: CupertinoColors.black,
+            rectangleColor: CupertinoColors.white,
+            text: 'continuar',
+          ),
+        ),
+        Spacer()
+      ]), //
     ); //
   }
 
   String? validateEmail(String? value) {
+    return null;
     if (value == null || value.isEmpty) {
       return 'Campo obrigatório'; // Retorna mensagem de erro se o campo estiver em branco
     }
