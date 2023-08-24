117199798cf0602e9efe6a8c5396a79e3d88d7a7
João Gabriel <joogps@gmail.com>
2023-08-24 09:05:37 -0300

    Criar ElasticButton

---
+++ lib/shared/widgets/elastic_button.dart
@@ -0,0 +1,54 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+class ElasticButton extends StatefulWidget {
+  Widget child;
+  final Function() onTap;
+
+  ElasticButton({required this.child, required this.onTap});
+
+  @override
+  _ElasticButtonState createState() => _ElasticButtonState();
+}
+
+class _ElasticButtonState extends State<ElasticButton> {
+  bool isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (_) {
+        setState(() {
+          isPressed = true;
+        });
+      },
+      onTapUp: (_) {
+        setState(() {
+          isPressed = false;
+        });
+      },
+      onTapCancel: () {
+        setState(() {
+          isPressed = false;
+        });
+      },
+      onTap: () {
+        widget.onTap();
+      },
+      child: AnimatedScale(
+          duration: Duration(milliseconds: 300),
+          curve: Curves.easeOutQuart,
+          scale: isPressed ? 0.985 : 1.0,
+          child: widget.child),
+    );
+  }
+
+  Matrix4 scaleXYZTransform({
+    double scaleX = 1.00,
+    double scaleY = 1.00,
+    double scaleZ = 1.00,
+  }) {
+    return Matrix4.diagonal3Values(scaleX, scaleY, scaleZ);
+  }
+}
