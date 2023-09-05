ba36f32bcc11e33f6a88ccb6b1d812cf7d0f082f
Ana Clara Castellain <111303609+fr0styb33@users.noreply.github.com>
2023-09-05 10:14:56 -0300

    Update user_detail_screen.dart

--- lib/features/user_detail/screens/user_detail_screen.dart
+++ lib/features/user_detail/screens/user_detail_screen.dart
@@ -19,46 +19,39 @@ class _UserDetailScreenState extends State<UserDetailScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        child: ElasticButton(
-            onTap: () {},
-            child: ClipOval(
-                child: BackdropFilter(
-                    filter: ImageFilter.blur(
-                      sigmaX: 6.0,
-                      sigmaY: 6.0,
-                    ),
-                    child: Container(
-                      child: Center(
-                        child: Padding(
-                          padding: const EdgeInsets.all(22.0),
-                          child: Image.asset(
-                            'assets/Star.png',
-                            color: CupertinoDynamicColor.resolve(
-                                CupertinoColors.label, context),
-                            opacity: const AlwaysStoppedAnimation(.90),
-                          ),
-                        ),
-                      ),
-                    )))));
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       child: Center(
-          child: SizedBox(
-        width: 200,
-        height: 80,
-        child: RoundButton(
-          onPressed: () async {
-            UserLoginProvider.shared.logout();
-            Navigator.pushNamed(context, "/onboarding");
-          },
-          rectangleColor: CupertinoColors.systemRed,
-          textColor: CupertinoColors.white,
-          text: 'logout',
-        ),
+          child: Column(
+        children: [
+          ElasticButton(
+              onTap: () {},
+              child: ClipOval(
+                child: Container(
+                  child: Center(
+                    child: Padding(
+                        padding: const EdgeInsets.all(22.0),
+                        child: Icon(
+                          CupertinoIcons.person_fill,
+                          color: CupertinoColors.black,
+                          size: 30.0,
+                        )),
+                  ),
+                ),
+              )),
+          SizedBox(
+            width: 200,
+            height: 80,
+            child: RoundButton(
+              onPressed: () async {
+                UserLoginProvider.shared.logout();
+                Navigator.pushNamed(context, "/onboarding");
+              },
+              rectangleColor: CupertinoColors.systemRed,
+              textColor: CupertinoColors.white,
+              text: 'logout',
+            ),
+          ),
+        ],
       )),
     );
   }
