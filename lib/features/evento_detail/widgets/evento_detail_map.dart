cdde05f811efd58e7c486709824580a3379c76e0
João Gabriel <joogps@gmail.com>
2023-09-05 12:02:38 -0300

    Atualizar mapa

--- lib/features/evento_detail/widgets/evento_detail_map.dart
+++ lib/features/evento_detail/widgets/evento_detail_map.dart
@@ -9,37 +9,56 @@ class EventoDetailMap extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: SizedBox(
-        height: 200,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(12),
-          child: Stack(children: [
-            FlutterMap(
+    return SizedBox(
+      height: 200,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: Stack(children: [
+          Container(
+            decoration: BoxDecoration(
+              color: color,
+            ),
+          ),
+          Opacity(
+            opacity: 0.97,
+            child: FlutterMap(
               options: MapOptions(
                 center: LatLng(-26.905926949896116, -49.07710147997988),
-                zoom: 17.0,
+                zoom: 15.0,
                 interactiveFlags: 0,
               ),
               nonRotatedChildren: [],
               children: [
                 TileLayer(
-                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  urlTemplate:
+                      'https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.png',
                   userAgentPackageName: 'com.joogps.role',
-                  retinaMode: true,
+                  // retinaMode: true,
+                ),
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      point: LatLng(-26.905926949896116, -49.07710147997988),
+                      width: 64,
+                      height: 64,
+                      builder: (context) => Icon(
+                        CupertinoIcons.circle_fill,
+                        color: color,
+                        size: 64,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-            Container(
-              decoration: BoxDecoration(
-                color: color,
-                backgroundBlendMode: BlendMode.softLight,
-                borderRadius: BorderRadius.circular(8),
-              ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: color,
+              backgroundBlendMode: BlendMode.softLight,
             ),
-          ]),
-        ),
+          ),
+        ]),
       ),
     );
   }
