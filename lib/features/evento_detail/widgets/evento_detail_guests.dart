257c17acc819adaa1401aeebf88cb4870410fe7c
caio fabio gregio <caio.f04@aluno.ifsc.edu.br>
2023-09-14 11:57:35 -0300

    tela bonita

--- lib/features/evento_detail/widgets/evento_detail_guests.dart
+++ lib/features/evento_detail/widgets/evento_detail_guests.dart
@@ -13,36 +13,58 @@ class EventDetailGuests extends StatelessWidget {
       "Convidado 5",
     ];
 
-    return SizedBox(
-      height: 100,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal, 
-        itemCount: guestList.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0), 
-            child: Column(
-              children: [
-                ClipOval(
-                  child: Container(
-                    width: 60, 
-                    height: 60,
-                    color: const Color.fromARGB(255, 100, 101, 102), 
-                    child: Image.asset("assets/Star.png"), 
-                  ),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  guestList[index],
-                  style: TextStyle(
-                    fontSize: 14, 
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
+    return Container(
+        decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 216, 212, 212),
+        borderRadius: BorderRadius.circular(12.0), 
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment:CrossAxisAlignment.start,
+          
+          children: [
+            Text("5 convidados",
+            style: TextStyle(fontWeight: FontWeight.bold,
+            fontSize: 26,),
+            
             ),
-          );
-        },
+          
+            SizedBox(
+              height: 100,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: guestList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      
+                      children: [
+                        ClipOval(
+                          child: Container(
+                            width: 60,
+                            height: 60,
+                            color: const Color.fromARGB(255, 100, 101, 102),
+                            child: Image.asset("assets/Star.png"),
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          guestList[index],
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
