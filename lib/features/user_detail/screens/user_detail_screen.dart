bb5a1b89f88f4a71ed44054ae412036974e485c4
Ana Clara Castellain <111303609+fr0styb33@users.noreply.github.com>
2023-09-14 10:45:04 -0300

    Arrumar tela de detalhe do usuário

--- lib/features/user_detail/screens/user_detail_screen.dart
+++ lib/features/user_detail/screens/user_detail_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:role/features/user_login/providers/user_login_provider.dart';
 import 'package:role/shared/widgets/round_button.dart';
 
@@ -11,73 +15,109 @@ class UserDetailScreen extends StatefulWidget {
 
 class _UserDetailScreenState extends State<UserDetailScreen> {
   bool showNewEvent = false;
-
   Duration duration = Duration(milliseconds: 200);
   Curve curve = Curves.easeInOutQuad;
+  File? image;
+
+  Future<void> pickImage(ImageSource gallery) async {
+    try {
+      final image = await ImagePicker().pickImage(source: gallery);
+      if (image == null) return;
+      final imageTemporary = File(image.path);
+      setState(() => this.image = imageTemporary);
+    } on PlatformException catch (e) {
+      print('Falha ao escolher imagem $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Detalhes do Usuário'),
+      ),
       child: Center(
-          child: Column(
-        children: [
-          ElasticButton(
-              onTap: () {
-                showCupertinoModalPopup(
-                    context: context,
-                    builder: ((context) {
-                      return CupertinoActionSheet(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                ElasticButton(
+                  onTap: () {
+                    showCupertinoModalPopup(
+                      context: context,
+                      builder: (context) {
+                        return CupertinoActionSheet(
                           cancelButton: CupertinoActionSheetAction(
                             onPressed: () {
                               Navigator.pop(context);
                             },
                             child: Text(
                               "Cancelar",
-                              style:
-                                  TextStyle(color: CupertinoColors.systemRed),
+                              style: TextStyle(
+                                color: CupertinoColors.systemRed,
+                              ),
                             ),
                           ),
                           actions: [
                             CupertinoActionSheetAction(
-                                onPressed: () {},
-                                child: Text("Escolher da biblioteca")),
+                              onPressed: () => pickImage(ImageSource.gallery),
+                              child: Text("Escolher da biblioteca"),
+                            ),
                             CupertinoActionSheetAction(
-                                onPressed: () {}, child: Text("Tirar foto")),
-                          ]);
-                    }));
-              },
-              child: ClipOval(
-                child: Container(
-                  child: Center(
-                    child: Padding(
-                        padding: const EdgeInsets.all(22.0),
-                        child: Icon(
-                          CupertinoIcons.person_fill,
-                          color: CupertinoColors.black,
-                          size: 30.0,
-                        )),
+                              onPressed: () => pickImage(ImageSource.camera),
+                              child: Text("Tirar foto"),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: ClipOval(
+                    child: Container(
+                      width: 100,
+                      height: 100,
+                      color: CupertinoColors.lightBackgroundGray,
+                      child: Center(
+                        child: image != null
+                            ? Image.file(
+                                image!,
+                                width: 90,
+                                height: 90,
+                                fit: BoxFit.cover,
+                              )
+                            : Icon(
+                                CupertinoIcons.person_fill,
+                                color: CupertinoColors.black,
+                                size: 50.0,
+                              ),
+                      ),
+                    ),
                   ),
                 ),
-              )),
-          CupertinoTextFormFieldRow(
-            placeholder: "Nome de Usuário",
-            placeholderStyle: TextStyle(),
-          ),
-          SizedBox(
-            width: 200,
-            height: 80,
-            child: RoundButton(
-              onPressed: () async {
-                UserLoginProvider.shared.logout();
-                Navigator.pushNamed(context, "/onboarding");
-              },
-              rectangleColor: CupertinoColors.systemRed,
-              textColor: CupertinoColors.white,
-              text: 'logout',
+                SizedBox(width: 20),
+                Expanded(
+                  child: CupertinoTextField(
+                    placeholder: "Nome de Usuário",
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      )),
+            SizedBox(
+              width: 200,
+              height: 80,
+              child: RoundButton(
+                onPressed: () async {
+                  UserLoginProvider.shared.logout();
+                  Navigator.pushNamed(context, "/onboarding");
+                },
+                rectangleColor: CupertinoColors.systemRed,
+                textColor: CupertinoColors.white,
+                text: 'logout',
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
