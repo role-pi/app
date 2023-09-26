491acff8c9b3af94a540bc1c4abf6cb480463933
João Gabriel <joogps@gmail.com>
2023-09-26 12:27:40 -0300

    Atualizar LoginProvider

--- lib/features/user_login/repository/user_repository.dart
+++ lib/features/user_login/repository/user_repository.dart
@@ -25,4 +25,46 @@ class UserRepository {
 
     return null;
   }
+
+  Future<bool?> signUp(String email) async {
+    try {
+      final response = await API().request(
+          endpoint: "usuario/signin", method: "POST", body: {"email": email});
+
+      Map decoded = json.decode(response.response);
+      if (decoded.containsKey("existing")) {
+        return decoded["existing"];
+      }
+    } catch (e) {
+      if (e is ApiError) {
+        print('Error Code: ${e.code}, Message: ${e.message}');
+      } else {
+        print('Unknown error occurred: $e');
+      }
+    }
+
+    return null;
+  }
+
+  Future<(Usuario?, String?)> verify(String email, String code) async {
+    try {
+      final response = await API().request(
+          endpoint: "usuario/verify",
+          method: "POST",
+          body: {"email": email, "code": code});
+
+      Map decoded = json.decode(response.response);
+      if (decoded.containsKey("user") && decoded.containsKey("token")) {
+        return (Usuario.fromJson(decoded["user"]), decoded["token"].toString());
+      }
+    } catch (e) {
+      if (e is ApiError) {
+        print('Error Code: ${e.code}, Message: ${e.message}');
+      } else {
+        print('Unknown error occurred: $e');
+      }
+    }
+
+    return (null, null);
+  }
 }
