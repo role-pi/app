3527e13631714bde2bd096b9efb1876a10cab370
rafael wolff <rafaelwolffluizx@gmail.com>
2023-10-19 11:51:08 -0300

    FirstLoginText

--- lib/features/user_login/screens/user_first_login_screen.dart
+++ lib/features/user_login/screens/user_first_login_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:role/features/user_login/widgets/verification_widget.dart';
 import 'package:role/features/user_login/providers/user_login_provider.dart';
 import 'package:role/features/user_login/widgets/signup_widget.dart';
+import 'package:role/shared/widgets/form/form_item_text_field.dart';
 
 class UserFirstLoginScreen extends StatefulWidget {
   @override
@@ -11,14 +13,29 @@ class UserFirstLoginScreen extends StatefulWidget {
 
 class _UserLoginScreenState extends State<UserFirstLoginScreen> {
   String? email;
+  var controller= TextEditingController()
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: CupertinoColors.white,
-      child: Column(
-        children: [],
-      )
+    return Stack(
+      children: [
+        Container(
+          color: CupertinoColors.white,
+        ),
+        Column(
+          children: [
+            FormItemTextField (
+              controller: controller,
+              title: "Email",
+              padding: EdgeInsets.all(4.0),
+              textSize: 16.0,
+              backgroundColor:
+              CupertinoColors.systemBackground, decoration: null,),
+          ]
+             )
+          ],
+        )
+      ],
     );
   }
 }
