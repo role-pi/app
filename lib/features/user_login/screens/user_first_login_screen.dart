6e364ba8de058988a6f9337583f8a6163d578dea
rafael wolff <rafaelwolffluizx@gmail.com>
2023-10-26 11:21:30 -0300

    Tela do FirstLogin

--- lib/features/user_login/screens/user_first_login_screen.dart
+++ lib/features/user_login/screens/user_first_login_screen.dart
@@ -5,6 +5,8 @@ import 'package:role/features/user_login/widgets/verification_widget.dart';
 import 'package:role/features/user_login/providers/user_login_provider.dart';
 import 'package:role/features/user_login/widgets/signup_widget.dart';
 import 'package:role/shared/widgets/form/form_item_text_field.dart';
+import 'package:role/shared/widgets/remote_profile_picture.dart';
+import 'package:role/shared/widgets/round_button.dart';
 
 class UserFirstLoginScreen extends StatefulWidget {
   @override
@@ -24,28 +26,55 @@ class _UserLoginScreenState extends State<UserFirstLoginScreen> {
           color: CupertinoColors.white,
         ),
         Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center, 
-              children: [
-                Text(
-                  "Tudo Pronto!",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: -1.5,
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center, 
+                children: [
+                  Text(
+                    "Tudo Pronto!",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: -1.5,
+                    ),
+                  ),
+                    SizedBox(
+                        height: 16,
+                    ),
+                  Text(
+                    textAlign: TextAlign.center,
+                    'Por último, gostaria de adicionar um nome e foto de perfil?',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Color.fromARGB(255, 98, 98, 100).withOpacity(0.8)
                   ),
                 ),
-               
-                Text(
-                  textAlign: TextAlign.center,
-                  'Por último, gostaria de adicionar um nome e foto de perfil?',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Color.fromARGB(255, 98, 98, 100).withOpacity(0.8)
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-            ],
-          )
+                Row(
+                  children: [
+                    RemoteProfilePicture(url: ""),
+                    SizedBox(
+                      width:20,
+                    ),
+                    Expanded(child: FormItemTextField(controller: controller, title: "Nome"))
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                RoundButton(
+                        text: "Finalizado",
+                        onPressed: () {},
+                        rectangleColor: Colors.black,
+                        textColor: Colors.white,
+                      ),
+                  ],
+                      ),
+            ),
+            
         ) 
       ]
     );
