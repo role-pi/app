78ce50f013c6ff1dc9fb9fe2a6f5b1cd2ef53513
Maiara Junches Seemann <maiarajunchesseemann@gmail.com>
2023-10-26 11:14:54 -0300

    Implementar insumo

--- lib/features/event_detail/widgets/event_detail_items.dart
+++ lib/features/event_detail/widgets/event_detail_items.dart
@@ -20,100 +20,116 @@ class EventDetailItems extends StatelessWidget {
       physics: NeverScrollableScrollPhysics(),
       itemCount: items.length,
       itemBuilder: (context, index) {
-        return Container(
-          padding: EdgeInsets.only(left: 16, right: 8, bottom: 16),
-          child: ElasticButton(
-            onTap: () {
-              Navigator.of(context).push(CupertinoPageRoute(
-                  builder: (context) => ItemsDetail(
-                        item: items[index],
-                      )));
-            },
-            child: Row(children: [
-              Stack(
-                children: <Widget>[
-                  Container(
-                    width: 50,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(8),
-                        bottomRight: Radius.circular(8),
+        Item item = items[index];
+        return Dismissible(
+          key: Key(item.id.toString()),
+          direction: DismissDirection.endToStart,
+          background: Stack(
+            alignment:Alignment.centerRight,
+            children: [
+              Container(decoration: BoxDecoration(color: CupertinoColors.destructiveRed, borderRadius: BorderRadius.circular(16))),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Icon(CupertinoIcons.trash, color: CupertinoColors.white, size: 32,),
+              ),
+
+            ],
+          ),
+          child: Container(
+            padding: EdgeInsets.only(left: 16, right: 8, bottom: 16),
+            child: ElasticButton(
+              onTap: () {
+                Navigator.of(context).push(CupertinoPageRoute(
+                    builder: (context) => ItemsDetail(
+                          item: items[index],
+                        )));
+              },
+              child: Row(children: [
+                Stack(
+                  children: <Widget>[
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(8),
+                          bottomRight: Radius.circular(8),
+                        ),
                       ),
-                    ),
-                    child: Center(
-                      child: Stack(
-                        clipBehavior: Clip.none,
-                        children: [
-                          Icon(
-                            CupertinoIcons.bubble_left_fill,
-                            color: CupertinoColors.systemGrey5
-                                .resolveFrom(context),
-                            size: 58,
-                          ),
-                          Positioned(
-                              bottom: 9,
-                              right: 8,
-                              child: Text(
-                                items[index].tipo.emoji,
-                                style: TextStyle(
-                                  fontSize: 26,
-                                ),
-                              )),
-                          Positioned(
-                            right: 32,
-                            bottom: 6,
-                            child: Container(
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    color: CupertinoColors.systemBackground
-                                        .resolveFrom(context),
-                                    width: 3,
+                      child: Center(
+                        child: Stack(
+                          clipBehavior: Clip.none,
+                          children: [
+                            Icon(
+                              CupertinoIcons.bubble_left_fill,
+                              color: CupertinoColors.systemGrey5
+                                  .resolveFrom(context),
+                              size: 58,
+                            ),
+                            Positioned(
+                                bottom: 9,
+                                right: 8,
+                                child: Text(
+                                  items[index].tipo.emoji,
+                                  style: TextStyle(
+                                    fontSize: 26,
                                   ),
-                                  borderRadius: BorderRadius.circular(32),
-                                ),
-                                child: Consumer<UserLoginProvider>(
-                                    builder: (context, provider, child) {
-                                  return RemoteProfilePicture(
-                                      url: provider.user?.profilePhoto,
-                                      size: 32);
-                                })),
-                          ),
-                        ],
+                                )),
+                            Positioned(
+                              right: 32,
+                              bottom: 6,
+                              child: Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: CupertinoColors.systemBackground
+                                          .resolveFrom(context),
+                                      width: 3,
+                                    ),
+                                    borderRadius: BorderRadius.circular(32),
+                                  ),
+                                  child: Consumer<UserLoginProvider>(
+                                      builder: (context, provider, child) {
+                                    return RemoteProfilePicture(
+                                        url: provider.user?.profilePhoto,
+                                        size: 32);
+                                  })),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(width: 16),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    items[index].nome,
-                    style: TextStyle(
-                      fontSize: 23,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: -1.0,
+                  ],
+                ),
+                SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      items[index].nome,
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: -1.0,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 2),
-                  Text(
-                    "há 2 horas",
-                    style: TextStyle(
-                      fontSize: 17,
-                      letterSpacing: -1.0,
-                      fontWeight: FontWeight.w600,
-                      color:
-                          CupertinoColors.secondaryLabel.resolveFrom(context),
+                    SizedBox(height: 2),
+                    Text(
+                      "há 2 horas",
+                      style: TextStyle(
+                        fontSize: 17,
+                        letterSpacing: -1.0,
+                        fontWeight: FontWeight.w600,
+                        color:
+                            CupertinoColors.secondaryLabel.resolveFrom(context),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(width: 12),
-              Spacer(),
-              ContainerText(text: "R\$ 60"),
-            ]),
+                  ],
+                ),
+                SizedBox(width: 12),
+                Spacer(),
+                ContainerText(text: "R\$ 60"),
+              ]),
+            ),
           ),
         );
       },
